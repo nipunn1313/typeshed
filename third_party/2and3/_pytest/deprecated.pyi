be86bc28aaf1be4be2cd8320525fd8a60c1e05e3
Nipunn Koorapati <nipunn@dropbox.com>
2020-02-24 15:27:10 -0800

    Add stubs for pytest

---
+++ third_party/2and3/_pytest/deprecated.pyi
@@ -0,0 +1,10 @@
+from _pytest.warning_types import PytestDeprecationWarning as PytestDeprecationWarning, UnformattedWarning as UnformattedWarning
+from typing import Any
+
+DEPRECATED_EXTERNAL_PLUGINS: Any
+FUNCARGNAMES: Any
+RESULT_LOG: Any
+FIXTURE_POSITIONAL_ARGUMENTS: Any
+NODE_USE_FROM_PARENT: Any
+JUNIT_XML_DEFAULT_FAMILY: Any
+NO_PRINT_LOGS: Any
